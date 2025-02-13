dcc95d3808632cfabb7cfb81d490b26312796bbd
Logadeep131 <Logadeep.Ramachandran@wissen.com>
2025-02-13 21:59:18 +0530

    initial commit

---
+++ Calculator/jenkinsdemo/src/main/java/jenkinsdemo/Calculator.java
@@ -0,0 +1,17 @@
+package jenkinsdemo;
+
+public class Calculator
+{
+	public void add(int a, int b)
+	{
+		System.out.println("Addition : "+(a+b));
+	}
+	public void sub(int a, int b)
+	{
+		System.out.println("Subtract : "+(a-b));
+	}
+	public void mul(int a, int b)
+	{
+		System.out.println("Multiply : "+(a * b));
+	}
+}
---
+++ Calculator/jenkinsdemo/src/main/java/jenkinsdemo/CalculatorMain.java
@@ -0,0 +1,15 @@
+package jenkinsdemo;
+
+
+public class CalculatorMain
+{
+	public static void main(String args[])
+	{
+		System.out.println("Calculator Operations......");
+ 
+		Calculator c1 = new Calculator();
+		c1.add(10, 20);
+		c1.sub(10, 20);
+		c1.mul(10, 20);
+	}
+}
